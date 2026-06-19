2c907502556b5a77d4cf3879481f63d4914c2ad0
DanikZhuk <danilkazhuk2006@yandex.ru>
2026-06-19 14:39:10 +0300

    fix: SceneFixes

--- Assets/Scripts/Core/SaveSystem/SaveDataService.cs
+++ Assets/Scripts/Core/SaveSystem/SaveDataService.cs
@@ -1,7 +1,6 @@
 using System;
 using System.Collections.Generic;
 using System.IO;
-using System.Linq;
 using Core.SaveSystem.Data;
 using Gameplay.Businesses.Generic.Models;
 using Gameplay.Investments;
